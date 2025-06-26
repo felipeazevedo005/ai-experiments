253752b68ea39781ead37e9f44a49d3f9e6cbde6
Felipe Azevedo <felipe.azevedo005@protonmail.com>
2025-06-26 16:56:15 -0300

    first commit

---
+++ lance-db.py
@@ -0,0 +1,34 @@
+import lancedb
+from lancedb.pydantic import LanceModel, Vector
+from lancedb.embeddings import get_registry
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db = lancedb.connect(os.path.dirname(__file__) + "/data")
+func = get_registry().get("openai").create(name="text-embedding-ada-002")
+
+
+class Words(LanceModel):
+    text: str = func.SourceField()
+    vector: Vector(func.ndims()) = func.VectorField()
+
+
+table = db.create_table("words", schema=Words, mode="overwrite")
+table.add(
+    [
+        {"text": "hello world"},
+        {"text": "barbie"},
+        {"text": "uno"},
+        {"text": "goodbye world"},
+        {"text": "JavaScript"},
+        {"text": "Java"},
+        {"text": "PHP"},
+        {"text": "Python"},
+    ]
+)
+
+query = "7 cartas"
+actual = table.search(query).limit(3).to_pandas()
+print(actual)
